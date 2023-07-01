20640638134b4bc917f41fd2b8d83c0ba6b0d50b
Vishakha <vishakhahegde23@gmail.com>
2023-07-02 01:59:45 +0530

    added forms

---
+++ front-end/src/components/Pages/Mentee.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MenteeForm from "../menteeform";
+
+const Mentee = () => {
+    return ( 
+        <div>
+        <h1>MENTEE</h1>
+        <MenteeForm/>
+        </div>
+     );
+}
+ 
+export default Mentee;
