ddd737820682fec6696a290a29da624d355ce202
Swapnil Jain <spj5533@gmail.com>
2023-07-01 22:08:41 +0530

    Adding model for Mongoose

---
+++ Backend/mentee.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const MenteeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  reason: {
+    type: String,
+    required: true
+  },
+  education:{
+    type:String,
+    required: true
+  },
+  my_intrests:{
+    type:String,
+    required: true
+  },
+  mode:{
+    type: Boolean,
+    required: true
+  },
+});
+
+const MenteeModel = mongoose.model('Mentee', MenteeSchema);
+
+module.exports = MenteeModel;
---
+++ Backend/volunteer.model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const MenteeSchema = new mongoose.Schema({
+  preference: {
+    type: String,
+    required: true
+  },
+  week: {
+    type: Date,
+    required: true
+  },
+  educational_qualification: {
+    type: String,
+    required: true
+  },
+   aadhar_no:{
+    type:Number,
+    required: true
+  },
+  skills:{
+    type:String,
+    required: true
+  },
+});
+
+const VolunteerModel = mongoose.model('Volunteer', VolunteerSchema);
+
+module.exports = VolunteerModel;
