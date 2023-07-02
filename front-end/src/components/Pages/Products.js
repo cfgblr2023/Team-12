462bd3bdc183440171c4bd69a44d29eee953a303
Tejasnamikaze <95167495+Tejasnamikaze@users.noreply.github.com>
2023-07-02 05:46:42 +0530

    donation

--- front-end/src/components/Pages/Products.js
+++ front-end/src/components/Pages/Products.js
@@ -1,17 +1,20 @@
 
 import React from 'react';
 import '../../App.css';
-import Cards from '../Cards';
-// import Footer from '../Footer';
+import Donate from '/Users/tejasm/Desktop/jpmc/Team-12/front-end/src/Donate.js';
+
+// import Cards from '../Cards';
+import Footer from '../Footer';
 import HeroSection from '../HeroSection-2';
 // import Footer from '../Footer';
 
 function Products() {
   return (
     <>
-      <HeroSection/>
-       <Cards />
-    
+      <Donate />
+
+      <HeroSection />
+      <footer />
     </>
   );
 }
